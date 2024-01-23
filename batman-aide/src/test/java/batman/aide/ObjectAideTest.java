0d5a810ef407873ba295047ba9e6db4084314d44
Kenwot <kenwot@msn.com>
2024-01-23 20:25:33 +0800

    ObjectAide

--- batman-aide/src/test/java/batman/aide/ObjectAideTest.java
+++ batman-aide/src/test/java/batman/aide/ObjectAideTest.java
@@ -17,10 +17,14 @@
 package batman.aide;
 
 import batman.aide.consts.ArrayConst;
+import batman.aide.exception.CloneFailedException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * {@link ObjectAide} 单元测试
@@ -33,6 +37,40 @@ public class ObjectAideTest {
     private static final String FOO = "foo";
     private static final String BAR = "bar";
     private static final String[] NON_EMPTY_ARRAY = { FOO, BAR, };
+
+    /**
+     * String that is cloneable.
+     */
+    static final class CloneableString implements Cloneable, Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        private final String value;
+
+        CloneableString(final String s) {
+            this.value = s;
+        }
+
+        @Override
+        public CloneableString clone() throws CloneNotSupportedException {
+            return (CloneableString) super.clone();
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+    }
+
+    static final class UncloneableString implements Cloneable, Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        private final String value;
+
+        UncloneableString(final String s) {
+            this.value = s;
+        }
+    }
 
     /**
      * Tests for {@link ObjectAide#allNonnull(Object...)}.
@@ -133,5 +171,52 @@ public class ObjectAideTest {
         assertTrue(ObjectAide.isArray(ArrayConst.EMPTY_THROWABLE_ARRAY));
         assertTrue(ObjectAide.isArray(ArrayConst.EMPTY_TYPE_ARRAY));
     }
+
+    /**
+     * Tests {@link ObjectAide#clone(Object)} with a cloneable object.
+     */
+    @Test
+    public void testCloneOfCloneable() {
+        final CloneableString string = new CloneableString("batman");
+        final CloneableString stringClone = ObjectAide.clone(string);
+        assertEquals("batman", stringClone.getValue());
+    }
+
+    /**
+     * Tests {@link ObjectAide#clone(Object)} with a not cloneable object.
+     */
+    @Test
+    public void testCloneOfNotCloneable() {
+        final String string = "batman";
+        assertNull(ObjectAide.clone(string));
+    }
+
+    /**
+     * Tests {@link ObjectAide#clone(Object)} with an array of primitives.
+     */
+    @Test
+    public void testCloneOfPrimitiveArray() {
+        assertArrayEquals(new int[]{1}, ObjectAide.clone(new int[]{1}));
+    }
+
+    /**
+     * Tests {@link ObjectAide#clone(Object)} with an object array.
+     */
+    @Test
+    public void testCloneOfStringArray() {
+        assertTrue(Arrays.deepEquals(
+                new String[]{"string"}, ObjectAide.clone(new String[]{"string"})));
+    }
+
+    /**
+     * Tests {@link ObjectAide#clone(Object)} with an uncloneable object.
+     */
+    @Test
+    public void testCloneOfUncloneable() {
+        final UncloneableString string = new UncloneableString("batman");
+        final CloneFailedException e = assertThrows(CloneFailedException.class, () -> ObjectAide.clone(string));
+        assertEquals(NoSuchMethodException.class, e.getCause().getClass());
+    }
+
 
 }
